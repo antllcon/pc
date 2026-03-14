7f3163505d042a29170b7ebddce4212773f1fb5f
antlllcon <pokemonivan32@gmail.com>
2026-03-14 09:47:40 +0300

    [main] Улучшил логирование в консоль

--- src/logger/logger/ConsoleLogger.h
+++ src/logger/logger/ConsoleLogger.h
@@ -7,6 +7,7 @@ class ConsoleLogger final : public ILogger
 {
 public:
 	explicit ConsoleLogger(bool isEnabled = true);
+	~ConsoleLogger() override = default;
 
 	void Log(const std::string& message) override;
 	void SetEnabled(bool isEnabled) override;
