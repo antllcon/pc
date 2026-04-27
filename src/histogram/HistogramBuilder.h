e04ee44dbf17381582949c3c69253fdaff9a61bc
antlllcon <pokemonivan32@gmail.com>
2026-04-27 14:04:37 +0300

    [histogram] Доработал интерфейс

--- src/histogram/HistogramBuilder.h
+++ src/histogram/HistogramBuilder.h
@@ -6,4 +6,7 @@
 namespace HistogramBuilder
 {
 Histogram Build(const Image& image);
+Histogram BuildAtomicInterleaved(const Image& image, unsigned int threadCount);
+Histogram BuildAtomicBlocked(const Image& image, unsigned int threadCount);
+Histogram BuildLocalHistograms(const Image& image, unsigned int threadCount);
 } // namespace HistogramBuilder
