6238a8382e00dc6dd796d64dcc46d3a432fc8dd2
antlllcon <pokemonivan32@gmail.com>
2026-04-27 12:52:19 +0300

    [histogram] Доработал интерфейс вывода изображения

--- src/image/Image.h
+++ src/image/Image.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <ostream>
 #include <string>
 
 class Image
 {
 public:
+	struct View
+	{
+		const Image& image;
+		unsigned int count;
+	};
+
 	explicit Image(const std::string& path);
 	~Image();
 
@@ -20,6 +27,8 @@ public:
 	const unsigned char* GetData() const;
 	unsigned char* GetData();
 
+	View operator()(unsigned int count) const;
+
 private:
 	void Load(const std::string& path);
 	void Free() noexcept;
@@ -30,3 +39,6 @@ private:
 	unsigned int m_channels = 0;
 	unsigned char* m_data = nullptr;
 };
+
+std::ostream& operator<<(std::ostream& os, const Image::View& view);
+std::ostream& operator<<(std::ostream& os, const Image& image);
