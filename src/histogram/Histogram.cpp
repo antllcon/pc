164a924493236c38f03d9e3c7596cd0d432c7dd9
antlllcon <pokemonivan32@gmail.com>
2026-05-04 13:09:51 +0300

    [histogram] Исправил вывод гистограммы в файл в виде таблицы CSV

--- src/histogram/Histogram.cpp
+++ src/histogram/Histogram.cpp
@@ -1,14 +1,19 @@
 #include "Histogram.h"
 
+namespace
+{
+constexpr std::string_view SEPARATOR = ";";
+}
+
 std::ostream& operator<<(std::ostream& os, const Histogram& histogram)
 {
 	os << "Intensity,Red,Green,Blue" << std::endl;
 
 	for (size_t i = 0; i < BinCount; ++i)
 	{
-		os << i << ","
-		   << histogram.r[i] << ","
-		   << histogram.g[i] << ","
+		os << i << SEPARATOR
+		   << histogram.r[i] << SEPARATOR
+		   << histogram.g[i] << SEPARATOR
 		   << histogram.b[i] << std::endl;
 	}
 
