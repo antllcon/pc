73f0b562523c4d6175afd298bcd3c029742a6a55
antlllcon <pokemonivan32@gmail.com>
2026-05-08 20:01:22 +0300

    [coroutines] Почистил проект и подключил boost asio с cobalt для коррутин

--- src/console/ConsoleArgs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <filesystem>
-
-namespace ConsoleArgs
-{
-std::filesystem::path ExtractImagePath(int argc, char* argv[]);
-} // namespace ConsoleArgs
--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,24 @@
-#include "console/ConsoleArgs.h"
-#include "console/ConsoleEncoding.h"
-#include "exporter/FileHelper.h"
-#include "histogram/Histogram.h"
-#include "histogram/HistogramBuilder.h"
-#include "image/Image.h"
-#include "logger/logger/ConsoleLogger.h"
-#include "logger/timer/ScopedTimer.h"
+#include "utils/console/ConsoleEncoding.h"
+#include "utils/logger/timer/ScopedTimer.h"
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
 
 #include <filesystem>
 #include <iostream>
-#include <memory>
 #include <syncstream>
 
-namespace
-{
-std::filesystem::path BuildOutputPath(const std::filesystem::path& imagePath)
-{
-	return imagePath.parent_path() / (imagePath.stem().string() + "-stats.csv");
-}
-} // namespace
+using namespace boost;
 
-int main(const int argc, char* argv[])
+int main()
 {
 	ConsoleEncoding consoleEncoding;
+
 	try
 	{
-		auto logger = std::make_shared<ConsoleLogger>(true);
-
-		auto imagePath = ConsoleArgs::ExtractImagePath(argc, argv);
-		auto outputPath = BuildOutputPath(imagePath);
-		auto image = Image(imagePath.string());
-
-		const unsigned int threadCount = std::thread::hardware_concurrency();
-
-		{
-			ScopedTimer timer("Однопоточное построение", logger);
-			auto histogram = HistogramBuilder::Build(image, false);
-			io::Save(outputPath, histogram);
-		}
-
-		{
-			ScopedTimer timer("Многопоточное (Interleaved)", logger);
-			auto histogram = HistogramBuilder::BuildAtomicInterleaved(image, threadCount);
-		}
-
-		{
-			ScopedTimer timer("Многопоточное (Blocked)", logger);
-			auto histogram = HistogramBuilder::BuildAtomicBlocked(image, threadCount);
-		}
-
-		{
-			ScopedTimer timer("Многопоточное (Local histograms)", logger);
-			auto histogram = HistogramBuilder::BuildLocalHistograms(image, threadCount);
-		}
+		ScopedTimer timer("Работа программы");
+		asio::io_context ctx;
+		ctx.run();
 	}
 	catch (const std::exception& e)
 	{
