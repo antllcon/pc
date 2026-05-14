d9f09af6a50ac299678e9853b61229807851db7b
antlllcon <pokemonivan32@gmail.com>
2026-05-14 09:38:30 +0300

    [stack] Сохранил реализацию

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,63 @@
-#include "utils/console/ConsoleEncoding.h"
-#include "utils/logger/timer/ScopedTimer.h"
-
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/steady_timer.hpp>
-
-#include <filesystem>
+#include <cds/container/treiber_stack.h>
+#include <cds/gc/hp.h>
+#include <cds/init.h>
+#include <cds/threading/model.h>
 #include <iostream>
-#include <syncstream>
 
-using namespace boost;
+struct EliminationTraits : cds::container::treiber_stack::make_traits<cds::opt::enable_elimination<true>>::type
+{
+};
+
+using BasicTreiberStack = cds::container::TreiberStack<cds::gc::HP, int>;
+using EliminationStack = cds::container::TreiberStack<cds::gc::HP, int, EliminationTraits>;
+
+namespace
+{
+void RunBasicStackTest()
+{
+	BasicTreiberStack stack;
+
+	stack.push(67);
+	stack.push(67);
+
+	int value = 0;
+	if (stack.pop(value))
+	{
+		std::cout << "Basic Stack Pop: " << value << std::endl;
+	}
+}
+
+void RunEliminationStackTest()
+{
+	EliminationStack stack;
+
+	stack.push(670);
+	stack.push(670);
+
+	int value = 0;
+	if (stack.pop(value))
+	{
+		std::cout << "Elimination Stack Pop: " << value << std::endl;
+	}
+}
+} // namespace
 
 int main()
 {
-	ConsoleEncoding consoleEncoding;
+	cds::Initialize();
 
-	try
 	{
-		ScopedTimer timer("Работа программы");
-		asio::io_context ctx;
-		ctx.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::osyncstream(std::cerr) << "[Error]\t" << e.what() << std::endl;
-		return EXIT_FAILURE;
+		// TODO: RAII обертки надо бы
+		cds::gc::HP hazardPointers;
+		cds::threading::Manager::attachThread();
+
+		RunBasicStackTest();
+		RunEliminationStackTest();
+
+		cds::threading::Manager::detachThread();
 	}
 
-	return EXIT_SUCCESS;
+	cds::Terminate();
+
+	return 0;
 }
